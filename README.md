46356907ec641376f470e68cfcce2704aefc4e57
thisisaaronland <aaron@localhost>
2021-09-14 16:41:18 -0700

    update to support custom dates

--- README.md
+++ README.md
@@ -41,25 +41,38 @@ func main(){
 Print the local time as well as the time in one or more timezones.
 
 ```
-> ./bin/time -h
+$> ./bin/time -h
 Print the local time as well as the time in one or more timezones.
 
 Usage:
 	 ./bin/time [options]
 
 Valid options are:
+  -date string
+    	YYYY-MM-dd HH:mm. If empty the current time in the computer's locale will be used.
   -in value
     	Zero or more strings to test whether they are contained by a given timezone's longform (major/minor) label.
+  -timezone string
+    	A valid major/minor timezone location. Required if -date is not empty.
 ```
 
 #### Example:
 
 ```
-$> ./bin/time -in Melbourne -in Cairo -in Honolulu -in China -in Montreal -in London
-Pacific/Honolulu         HST	Tuesday    2021-09-14 06:01
-Local                    PDT	Tuesday    2021-09-14 09:01 👈
-America/Montreal         EDT	Tuesday    2021-09-14 12:01
-Europe/London            BST	Tuesday    2021-09-14 17:01
-Africa/Cairo             EET	Tuesday    2021-09-14 18:01
-Australia/Melbourne      AEST	Wednesday  2021-09-15 02:01
+$> ./bin/time  -in Melbourne -in Cairo -in Honolulu -in China -in Montreal -in London
+Pacific/Honolulu         HST	Tuesday    2021-09-14 13:38
+👉                       PDT	Tuesday    2021-09-14 16:38 👈
+America/Montreal         EDT	Tuesday    2021-09-14 19:38
+Europe/London            BST	Wednesday  2021-09-15 00:38
+Africa/Cairo             EET	Wednesday  2021-09-15 01:38
+Australia/Melbourne      AEST	Wednesday  2021-09-15 09:38
+```
+
+Or specifying a custom date in a timezone:
+
+```
+$> ./bin/time  -date '2021-09-15 03:00' -timezone 'Asia/Singapore' -in London -in 'Los_Angeles'
+America/Los_Angeles      PDT	Tuesday    2021-09-14 12:00
+Europe/London            BST	Tuesday    2021-09-14 20:00
+👉                       +08	Wednesday  2021-09-15 03:00 👈
 ```
--- cmd/time/main.go
+++ cmd/time/main.go
@@ -5,10 +5,12 @@ import (
 	"context"
 	"fmt"
 	"github.com/aaronland/go-world-clock"
-	"github.com/sfomuseum/go-flags/flagset"
+	"github.com/sfomuseum/go-flags/flagset"	
 	"github.com/sfomuseum/go-flags/multi"
 	"log"
 	"os"
+	_ "strings"
+	"unicode/utf8"
 	"time"
 )
 
@@ -19,6 +21,9 @@ func main() {
 	var in_timezones multi.MultiString
 	fs.Var(&in_timezones, "in", "Zero or more strings to test whether they are contained by a given timezone's longform (major/minor) label.")
 
+	date := fs.String("date", "", "YYYY-MM-dd HH:mm. If empty the current time in the computer's locale will be used.")
+	timezone := fs.String("timezone", "", "A valid major/minor timezone location. Required if -date is not empty.")
+	
 	fs.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Print the local time as well as the time in one or more timezones.\n\n")
 		fmt.Fprintf(os.Stderr, "Usage:\n\t %s [options]\n\n", os.Args[0])
@@ -34,13 +39,37 @@ func main() {
 		Timezones: in_timezones,
 	}
 
-	now := time.Now()
-	here := now.Local()
+	var source time.Time
 
-	err := process(ctx, here, filters)
+	if *date != "" {
+
+		if *timezone == "" {
+			log.Fatalf("Missing -timezone parameter")
+		}
+		
+		loc, err := time.LoadLocation(*timezone)
+			
+		if err != nil {
+			log.Fatalf("Failed load location '%s', %v", *timezone, err)
+		}
+		
+		t, err := time.ParseInLocation("2006-01-02 15:04", *date, loc)
+
+		if err != nil {
+			log.Fatalf("Failed to parse '%s', %v", *date, err)
+		}
+
+		source = t
+		
+	} else {
+		now := time.Now()
+		source = now.Local()
+	}
+	
+	err := process(ctx, source, filters)
 
 	if err != nil {
-		log.Fatalf("Failed to process '%v', %v", here, err)
+		log.Fatalf("Failed to process '%v', %v", source, err)
 	}
 }
 
@@ -65,8 +94,8 @@ func process(ctx context.Context, source time.Time, filters *clock.Filters) erro
 		if r_zn == zn {
 
 			if !seen {
-				label := "Local"
-				label = padding(label, 24)
+				label := "👉"
+				label = padding(label, 23)
 
 				d := r.Time.Format(d_fmt)
 				d = padding(d, 10)
@@ -100,12 +129,16 @@ func process(ctx context.Context, source time.Time, filters *clock.Filters) erro
 
 func padding(input string, final int) string {
 
-	input_len := len(input)
+	// input_len := len(input)
+	input_len := utf8.RuneCountInString(input)
+	
+	// log.Println(input, input_len)
 	padding := ""
 
-	for i := 0; i < final-input_len; i++ {
+	for i := 0; i < final - input_len; i++ {
 		padding = padding + " "
 	}
 
+	//log.Println(input, len(padding))
 	return input + padding
 }
